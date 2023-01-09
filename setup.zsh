48b2cef50f7999361735c873c89d663a60158772
Andy Stoerkel <astoer1@students.towson.edu>
2023-01-09 11:32:30 -0500

    added install script with makefile

---
+++ setup.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+echo "hello world"
+brew install chruby ruby-install xz
+xcode-select --install
+ruby-install ruby-3.1.3
+echo "source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh" >> ~/.zshrc
+echo "source $(brew --prefix)/opt/chruby/share/chruby/auto.sh" >> ~/.zshrc
+echo "chruby ruby-3.1.3" >> ~/.zshrc
+gem install jekyll bundler
+bundle install
+source ~/.zshrc
