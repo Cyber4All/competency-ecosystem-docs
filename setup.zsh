222fceb6561c0b67179b28bbc3fd15c3961a3b05
Andrew Stoerkel <41537999+AStoerkel1@users.noreply.github.com>
2023-01-10 09:48:55 -0500

    updated order

--- setup.zsh
+++ setup.zsh
@@ -5,6 +5,7 @@ ruby-install ruby-3.1.3
 echo "source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh" >> ~/.zshrc
 echo "source $(brew --prefix)/opt/chruby/share/chruby/auto.sh" >> ~/.zshrc
 echo "chruby ruby-3.1.3" >> ~/.zshrc
+source ~/.zshrc
 gem install jekyll bundler
 bundle install
-source ~/.zshrc
+
